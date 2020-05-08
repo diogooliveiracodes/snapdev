15a5370d5f563f90c9d79f22a1666cbfee4795b1
Diogo <diogo.o@icloud.com>
2020-05-08 19:26:07 -0300

    email5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -241,7 +241,7 @@
                     })
 
                 $.ajax({
-                    url: "{{ https://snapdev.herokuapp.com/orcamento' }}",
+                    url: "https://snapdev.herokuapp.com/orcamento",
                     type: "post",
                     data: $(this).serialize(),
                     dataType: "json",
