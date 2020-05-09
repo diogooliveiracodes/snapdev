2eb862dd78c9f60078406b629381ba92d1673db1
Diogo <diogo.o@icloud.com>
2020-05-09 13:50:22 -0300

    dominio 6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -241,7 +241,7 @@
                     })
 
                 $.ajax({
-                    url: {{ route('orcamento') }},
+                    url: '{{ route("orcamento") }}',
                     type: "post",
                     data: $(this).serialize(),
                     dataType: "json",
